75177d92d7f5be769453f3436110e66982610f26
Mohammad Z. Hossain (Nokia) <mohammad.z.hossain@nokia.com>
2024-06-27 23:40:30 +0300

    project-setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>My todo Application</h1>
+      <Button>Hellow Button</Button>
+    </>
+  );
+}
+
+export default App;
