638edd0b2243b030b8cfb34a85c0d205c5da7a1b
Mohammad Z. Hossain (Nokia) <mohammad.z.hossain@nokia.com>
2024-07-03 00:54:07 +0300

    modal-button-from-shadcn

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,16 +1,23 @@
+import { Button } from "../ui/button";
+import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
   return (
     <div>
-      <div>
-        <button>Add todo</button>
+      <div className="flex justify-between mb-5">
+        <AddTodoModal></AddTodoModal>
         <button>Filter</button>
       </div>
-      <div className="bg-red-500 w-full h-full h-[500px] rounded-xl p-5 space-y-3">
+      <div className="bg-primary-gradient w-full h-full rounded-xl space-y-3 p-[5px]">
         {/* <div className="bg-white text-2xl font-bold  p-3 flex justify-center items-center rounded-md">
           <p>There is no pending task..</p>
         </div> */}
+        <div className="bg-white p-5 w-full h-full rounded-lg">
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/todo/TodoFilter.tsx
@@ -0,0 +1,31 @@
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+
+const TodoFilter = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">Open</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+          <DropdownMenuRadioItem value="top">Top</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="bottom">Bottom</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="right">Right</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default TodoFilter;
